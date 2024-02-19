dd5d53d4a1e6348d348cfe625fba20529a1bcab6
Lau1907 <130590443+Lau1907@users.noreply.github.com>
2024-02-19 16:14:54 -0600

    cambios

---
+++ client/src/app/components/boss-panel/boss-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boss-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './boss-panel.component.html',
+  styleUrl: './boss-panel.component.css'
+})
+export class BossPanelComponent {
+
+}
