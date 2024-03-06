867fda6bcbf5ff70e237e5fd808e3445bb9dcf8a
Darka <darkangel13gamer@gmail.com>
2024-03-06 13:23:34 -0600

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HistActComponent } from './components/hist-act/hist-act.component';
-
+import { AutosComponent } from './components/autos/autos.component'; // Reemplaza 'ruta/al/' por la ruta real de tu componente AutosComponent
+import { PromocionesComponent } from './components/promociones/promociones.component';
 const routes: Routes = [
   {
     path : '',
@@ -11,7 +12,13 @@ const routes: Routes = [
   {
     path : 'home',
     component : HistActComponent
-  }
+  },
+  { path: 'autos',
+   component: AutosComponent
+  },
+  { path: 'promociones',
+    component: PromocionesComponent
+  } 
   ];
 
 @NgModule({
