6a7939e57df32cf0bb1a328186c1e959b60ba4a6
Brayanpower <116902837+Brayanpower@users.noreply.github.com>
2024-03-09 15:27:23 -0600

    Revert "Creaccion front y pais router"
    This reverts commit 5ecdd8cc8fd46073b0f2168852769068de479885.

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,9 +2,8 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 
-//const indexRoutes = require('./routes/index'); 
-const Vehiculos = require('./routes/vehiculosRoutes'); 
-const Paises = require('./routes/PaisesRouter'); 
+const indexRoutes = require('./routes/index'); 
+const taks = require('./routes/vehiculosRoutes'); 
 // settings
 app.set('port', process.env.PORT || 3000);
 
@@ -17,17 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // routes
-//app.use(indexRoutes);
-app.use('/api',Vehiculos);
-app.use('/api',Paises);
-
-//Static Files
-
-
-
-
-
-//Start Server
+app.use(indexRoutes);
+app.use('/api',taks);
 app.listen(app.get('port'), () => {
   console.log('Server on port ', app.get('port'));
 });
