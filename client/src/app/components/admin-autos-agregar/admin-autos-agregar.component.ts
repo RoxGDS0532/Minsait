1c92d2fdc87a139e82a2d5f4024dd582125d652d
Darka <darkangel13gamer@gmail.com>
2024-03-09 15:11:23 -0600

    Create admin-autos-agregar.component.ts

---
+++ client/src/app/components/admin-autos-agregar/admin-autos-agregar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-autos-agregar',
+  templateUrl: './admin-autos-agregar.component.html',
+  styleUrls: ['./admin-autos-agregar.component.css']
+})
+export class AdminAutosAgregarComponent {
+
+}
